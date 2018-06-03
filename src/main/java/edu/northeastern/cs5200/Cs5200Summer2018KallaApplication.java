de8a3be980bc2773536abe1325b86337adb5b981
ryankalla <ryankalla94@gmail.com>
2018-06-03 12:39:31 -0400

    Integrating Spring Boot with MySQL

--- src/main/java/edu/northeastern/cs5200/Cs5200Summer2018KallaApplication.java
+++ src/main/java/edu/northeastern/cs5200/Cs5200Summer2018KallaApplication.java
@@ -2,9 +2,16 @@ package edu.northeastern.cs5200;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Cs5200Summer2018KallaApplication {
+public class Cs5200Summer2018KallaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Cs5200Summer2018KallaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5200Summer2018KallaApplication.class, args);
--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
@@ -1,7 +1,22 @@
 package edu.northeastern.cs5200.controllers.hello;
 
-public class HelloObject {
 
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+
+import javax.persistence.GenerationType;
+
+import javax.persistence.Id;
+
+
+@Entity(name="hello")
+public class HelloObject {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
 	private String message;
 
 	public HelloObject(String message) {
@@ -9,6 +24,14 @@ public class HelloObject {
 	}
 
 	public HelloObject() {
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getMessage() {
